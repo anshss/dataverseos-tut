801d57e04347e412255fd67e898ba06663236418
Ansh <anshsaxena4190@gmail.com>
2023-11-18 01:10:18 +0530

    first-commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,169 @@
-import Image from 'next/image'
+"use client";
+import {
+    DataverseConnector,
+    SYSTEM_CALL,
+    RESOURCE,
+} from "@dataverse/dataverse-connector";
+import React, { useState } from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    const [conditionAddress, setConditionAddress] = useState();
+    const [pkh, setPkh] = useState("");
+    const [inputs, setInputs] = useState({
+        text: "",
+        updateText: "",
+        fileId: "",
+    });
+
+    const [fetchedData, setFetchedData] = useState<any>();
+    const [fileId, setFileId] = useState<String | undefined>();
+    const [loading, setLoading] = useState(false);
+
+    const dataverseConnector = new DataverseConnector();
+
+    const login = async () => {
+        await createCapability();
+    };
+    
+    const createCapability = async () => {
+        await dataverseConnector.connectWallet();
+        const pkh = await dataverseConnector.runOS({
+            method: SYSTEM_CALL.createCapability,
+            params: {
+                appId: "f5661a5a-cbe3-4c00-862f-cf8f8ac38899",
+                resource: RESOURCE.CERAMIC,
+            },
+        });
+        console.log("pkh:", pkh);
+        setPkh(pkh);
+        return pkh;
+    };
+
+    const uploadWithDataverse = async () => {
+        setLoading(true);
+        const encrypted = JSON.stringify({
+            text: false,
+            images: false,
+            videos: false,
+        });
+
+        await dataverseConnector.connectWallet();
+        const res = await dataverseConnector.runOS({
+            method: SYSTEM_CALL.createIndexFile,
+            params: {
+                modelId:
+                    "kjzl6hvfrbw6c56aaaabngouspro7na03id4zdvvuax1lpakx48soehix9dnk69",
+                fileName: "test",
+                fileContent: {
+                    modelVersion: "0",
+                    text: inputs.text,
+                    images: [],
+                    videos: [],
+                    createdAt: new Date().toISOString(),
+                    updatedAt: new Date().toISOString(),
+                    encrypted,
+                },
+            },
+        });
+        // console.log("file id:", res.fileContent.file.contentId);
+        setFileId(res.fileContent.file.contentId);
+        setLoading(false);
+    };
+
+    const loadWithDataverse = async () => {
+        const res = await dataverseConnector.runOS({
+            method: SYSTEM_CALL.loadFile,
+            params: inputs.fileId,
+        });
+
+        setFetchedData(res.fileContent.content.text);
+        console.log("fetched", res.fileContent.content.text);
+
+        console.log(res);
+    };
+
+    const updateWithDataverse = async () => {
+        const encrypted = JSON.stringify({
+            text: false,
+            images: false,
+            videos: false,
+        });
+
+        await dataverseConnector.connectWallet();
+        const res = await dataverseConnector.runOS({
+            method: SYSTEM_CALL.updateIndexFile,
+            params: {
+                fileId: inputs.fileId,
+                fileName: "test",
+                fileContent: {
+                    modelVersion: "0",
+                    text: inputs.text,
+                    images: [],
+                    videos: [],
+                    createdAt: new Date().toISOString(),
+                    updatedAt: new Date().toISOString(),
+                    encrypted,
+                },
+            },
+        });
+        console.log("file id:", res);
+        setFileId(res.fileContent.file.contentId);
+    };
+
+    return (
+        <div>
+            <div className=" mb-4">
+                {pkh ? (
+                    <p>{pkh}</p>
+                ) : (
+                    <button onClick={login}>Connect Wallet</button>
+                )}
+            </div>
+            <div className=" mb-4">
+                <p>Upload File</p>
+                <input
+                    type="text"
+                    className="text-black"
+                    onChange={(e) =>
+                        setInputs({ ...inputs, text: e.target.value })
+                    }
+                />
+                <button onClick={uploadWithDataverse}>Upload</button>
+                {loading ? <p>Loading..</p> : <p>file id: {fileId}</p>}
+            </div>
+            <div className=" mb-4">
+                <p>Update File</p>
+                <input
+                    type="text"
+                    className="text-black"
+                    placeholder="file id"
+                    onChange={(e) =>
+                        setInputs({ ...inputs, fileId: e.target.value })
+                    }
+                />
+                <input
+                    type="text"
+                    className="text-black"
+                    placeholder="inputs"
+                    onChange={(e) =>
+                        setInputs({ ...inputs, updateText: e.target.value })
+                    }
+                />
+                <button onClick={updateWithDataverse}>Update</button>
+                <p>file id: {fileId}</p>
+            </div>
+            <div className=" mb-4">
+                <p>Load File</p>
+                <input
+                    type="text"
+                    className="text-black"
+                    onChange={(e) =>
+                        setInputs({ ...inputs, fileId: e.target.value })
+                    }
+                />
+                <button onClick={loadWithDataverse}>Load</button>
+                <p>data: {fetchedData}</p>
+            </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
 }
